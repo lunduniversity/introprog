90c419f73a87cf3eaf34f4c4928ddf63b4e51394
bjornregnell <bjorn.regnell@cs.lth.se>
2016-02-06 23:53:21 +0100

    refactor pre and post chapter files

--- plan/StringExtras.scala
+++ plan/StringExtras.scala
@@ -6,13 +6,27 @@ object StringExtras {
   }
   
     
-  implicit class StringSaver(contents: String) {
+  implicit class StringSaver(s: String) {
     import java.nio.file.{Paths, Files}
     import java.nio.charset.StandardCharsets.UTF_8
+    
     def save(fileName: String) = {
       println("Saving file: " + Paths.get(fileName))
-      Files.write(Paths.get(fileName), contents.getBytes(UTF_8))
+      Files.write(Paths.get(fileName), s.getBytes(UTF_8))
+    }
+  }
+  
+  implicit class StringLatexEscaper(s: String) {
+    def latexEscape = {
+      val escapeChars = "&_"
+      var result = s
+      for (c <- escapeChars){ 
+        result = result.replaceAllLiterally(c.toString, "\\" + c)
+      }
+      result
     }
   }
 
 }
+
+
