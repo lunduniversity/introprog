ef4a7ce609311e3731c43caaf844e09d963e3910
bjornregnell <bjorn.regnell@cs.lth.se>
2016-04-05 15:10:35 +0200

    update comments in prepend util

--- util/prep.sh
+++ util/prep.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# change CLASSDIR to correct absolute path:
+# change CLASSDIR to correct path to compiled class files from prepend.scala:
 CLASSDIR="/home/bjornr/github/lunduniversity/introprog/util/target/scala-2.11/classes"
 scala -cp $CLASSDIR prepend
