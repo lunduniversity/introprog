b4cff67454defc413815e595e74633bbf2184735
bjornregnell <bjorn.regnell@cs.lth.se>
2016-04-29 16:51:37 +0200

    add example DiceReg2 to show abstraction in Java

--- compendium/examples/DiceReg2.java
+++ compendium/examples/DiceReg2.java
@@ -1,11 +1,12 @@
-// DiceReg.java
+// DiceReg2.java
 import java.util.Random;
 
-public class DiceReg {
-    public static void main(String[] args) {
-        int[] diceReg = new int[6];  
+public class DiceReg2 {
+    public static int[] diceReg = new int[6];  
+    private static Random rnd = new Random();
+
+    public static int parseArguments(String[] args) {
         int n = 100;
-        Random rnd = new Random();
         if (args.length > 0) {
             n = Integer.parseInt(args[0]);
         } 
@@ -16,13 +17,26 @@ public class DiceReg {
             System.out.print(" with seed " + seed);
         } 
         System.out.println(".");
+        return n;        
+    }
+    
+    public static void registerPips(int n){
         for (int i = 0; i < n; i++) {
             int pips = rnd.nextInt(6);
             diceReg[pips]++; 
         }
+    }
+    
+    public static void printReg() {
         for (int i = 1; i <= 6; i++) {
             System.out.println("Number of " + i + "'s: " + 
                 diceReg[i-1]); 
         }
+    }
+
+    public static void main(String[] args) {
+        int n = parseArguments(args);
+        registerPips(n);
+        printReg();
     }
 }
