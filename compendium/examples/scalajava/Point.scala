5faba6c9e195bca62eb82af4ec3f819a3a5ec31b
bjornregnell <bjorn.regnell@cs.lth.se>
2016-06-21 18:03:54 +0200

    add examples for scalajava exercise

---
+++ compendium/examples/scalajava/Point.scala
@@ -0,0 +1,23 @@
+class Point(val x: Int, val y: Int, save: Boolean = false) {
+  import Point._
+  
+  if (save) saved.prepend(this)
+  
+  def this() = this(0, 0)
+  
+  def distanceTo(that: Point) = distanceBetween(this, that)
+  
+  override def toString = s"Point($x, $y)"
+}
+
+object Point {
+  import scala.collection.mutable.{ArrayBuffer, Buffer}
+  
+  private val saved: Buffer[Point] = ArrayBuffer.empty
+  
+  def distanceBetween(p1: Point, p2: Point) = 
+    math.hypot(p1.x - p2.x, p1.y - p2.y)    
+  
+  def showSaved: Unit = 
+    println(saved.mkString("Saved: ", ", ", "\n"))
+}
