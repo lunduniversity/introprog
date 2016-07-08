beb593ef8203f52f7be4912b69404f4846ca6805
hnrklssn <hnrklssn@gmail.com>
2016-07-08 12:06:54 +0200

    Revert "qwer"
    This reverts commit 9521eea84e73965fd3a187a210291c6bc09c4231.

--- workspace/w12_life/src/rules/CyclicRule.scala
+++
@@ -1,8 +0,0 @@
-package rules
-
-import models._
-
-// Implements the Cyclic cellular automaton.
-// https://en.wikipedia.org/wiki/Cyclic_cellular_automaton
-object CyclicRule extends Rule {
-  def apply(m: Matrix2D, i: Int, j: Int): Int = ???
