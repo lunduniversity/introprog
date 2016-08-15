f9467bd0fad5bee380a2d455ee674c5ffc75e93c
tejp <cyberkygen@gmail.com>
2016-08-15 04:13:58 +0200

    innehåll till handledning

--- workspace/w10_survey/src/main/scala/stats/Command.scala
+++ workspace/w10_survey/src/main/scala/stats/Command.scala
@@ -1,11 +1,9 @@
 package stats
 
-/**
- * Defines, creates and executes commands.
- */
+/** Defines, creates and executes commands. */
 object Command {
 
-  val List =
+  val list =
     """Commands: -save <filename>
       |          -sep <separator>
       |          -filter <column> <values: a b c>
@@ -15,30 +13,20 @@ object Command {
 
   type Command = Table => Table
 
-  case class InvalidCommandException(message: String) extends Exception(message)
-
   /**
-   * Takes arguments and matches them to a  single command or 
+   * Takes arguments and matches them to a single command or
    * throws an exception if no match is found.
-   * 
-   * @param args                     The arguments.
-   * @throws InvalidCommandException
    */
   def parseOne(args: Vector[String]): Command = ???
 
   /**
    * Turns a collection of arguments into a collection commands.
-   * 
-   * @param args The arguments.
    */
   def parseAll(args: Vector[String]): Vector[Command] = ???
 
   /**
    * Executes the specified commands in a chain. The input to the
    * next command is the output from the previous.
-   * 
-   * @param commands The collection of commands.
-   * @param table    The table to execute the commands with.
    */
   def runAllWith(commands: Vector[Command], table: Table): Table = ???
 }
--- workspace/w10_survey/src/main/scala/stats/Main.scala
+++ workspace/w10_survey/src/main/scala/stats/Main.scala
@@ -4,25 +4,22 @@ import scala.util.Try
 
 object Main {
 
-  val Usage =
-    s"Usage: <uri> <separator> [Commands]\n\n${Command.List}"
+  val usage =
+      s"Usage: <uri> <separator> [Commands]\n\n${Command.list}"
 
   def main(args: Array[String]): Unit = args.toVector match {
 
     case uri +: sep +: rest if rest != Vector() => {
       Try {
+    	  println(s"""Loading "$uri" with separator "$sep"...\n""")
         val t = Table.fromFile(uri, sep)
-        println(s"""Opened "$uri" with separator "$sep".""" +
-          "Size: ${t.dim._1}x${t.dim._2}.\n")
+        println(s"Done. Size: ${t.dim._1}x${t.dim._2}.\n")
         val commands = Command.parseAll(rest)
         Command.runAllWith(commands, t)
-
       }.recover {
-        // Add more cases here to handle individual exceptions.
-        case e: Exception => println(s"Error: $e")
+        case e: Exception => println(s"Error: $e\n\n$usage")
       }
     }
-
-    case _ => println(s"Not enough arguments.\n\n$Usage")
+    case _ => println(s"Not enough arguments.\n\n$usage")
   }
 }
