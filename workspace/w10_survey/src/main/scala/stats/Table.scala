c78f278176cf0104e6f66880223c32d353f2474c
tejp <cyberkygen@gmail.com>
2016-08-15 04:13:58 +0200

    innehåll till handledning

--- workspace/w10_survey/src/main/scala/stats/Table.scala
+++ workspace/w10_survey/src/main/scala/stats/Table.scala
@@ -7,7 +7,8 @@ package stats
  * @param headings Column headings.
  * @param sep      The String character that separates the columns.
  */
-case class Table(matrix: Vector[Vector[String]], headings: Vector[String], sep: String) {
+case class Table(
+    matrix: Vector[Vector[String]], headings: Vector[String], sep: String) {
 
   /**
    * Returns the width (columns) and height (rows) of the
@@ -15,11 +16,7 @@ case class Table(matrix: Vector[Vector[String]], headings: Vector[String], sep: 
    */
   val dim: (Int, Int) = ???
 
-  /**
-   * Returns the values from a single column.
-   *
-   *  @param c The column index.
-   */
+  /** Returns the values from a specified column. */
   def col(c: Int): Vector[String] = ???
 
   /** Returns the matrix in text format */
@@ -28,24 +25,17 @@ case class Table(matrix: Vector[Vector[String]], headings: Vector[String], sep: 
   /**
    * Returns a copy of the current table sorted according to
    * the specified column.
-   *
-   * @param c The column index.
    */
   def sort(c: Int): Table = ???
 
   /**
    * Returns a copy of the current table filtered according to
    * if the specified column is among the wanted values.
-   *
-   * @param c      The column index.
-   * @param wanted The wanted values.
    */
   def filter(c: Int, wanted: Vector[String]): Table = ???
 
   /**
    * Returns the frequency registered data from a specified column.
-   *
-   * @param c The column index.
    */
   def register(c: Int): Vector[(String, Int)] = ???
 }
@@ -53,19 +43,26 @@ case class Table(matrix: Vector[Vector[String]], headings: Vector[String], sep: 
 object Table {
 
   /**
-   * Opens and reads column separated text data from either
+   * Reads column separated text data from either
    * a file or a URL into a Table.
-   *
+   * 
    * @param uri The location of the data.
-   * @param sep The separator that distinguishes columns.
+   * @param sep The character that separates the columns.
    */
   def fromFile(uri: String, sep: String): Table = ???
 
   /**
    * Writes a text formatted Table to disk.
-   *
-   * @param path  The location of the file to write.
-   * @param table The table to be written.
    */
   def toFile(path: String, table: Table): Unit = ???
+
+  /**
+   * To test Table
+   */
+  def main(args: Array[String]): Unit = {
+    val table = Table.fromFile("src/main/resources/favorit.csv", ",")
+    val tablefs = table.filter(4, Vector("Linux")).sort(5)
+    println(tablefs.register(5).mkString("\n"))
+    toFile("src/main/resources/out.csv", tablefs)
+  }
 }
