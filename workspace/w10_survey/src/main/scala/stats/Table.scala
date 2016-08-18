3c7b82dc2c62c8d6f9abb7ac5c5eb223c20f8b74
tejp <cyberkygen@gmail.com>
2016-08-19 01:39:07 +0200

    fix remarks from testing

--- workspace/w10_survey/src/main/scala/stats/Table.scala
+++ workspace/w10_survey/src/main/scala/stats/Table.scala
@@ -10,10 +10,7 @@ package stats
 case class Table(
     matrix: Vector[Vector[String]], headings: Vector[String], sep: String) {
 
-  /**
-   * Returns the width (columns) and height (rows) of the
-   * matrix data.
-   */
+  /** Returns the width (columns) and height (rows) of the matrix data. */
   val dim: (Int, Int) = ???
 
   /** Returns the values from a specified column. */
@@ -22,20 +19,20 @@ case class Table(
   /** Returns the matrix in text format */
   override lazy val toString: String = ???
 
-  /**
-   * Returns a copy of the current table sorted according to
-   * the specified column.
-   */
+  /** Returns a copy of itself with the rows sorted on the specified column. */
   def sort(c: Int): Table = ???
 
   /**
-   * Returns a copy of the current table filtered according to
-   * if the specified column is among the wanted values.
+   * Returns a copy of itself with the rows filtered so that the given column
+   * only contains the wanted values.
    */
   def filter(c: Int, wanted: Vector[String]): Table = ???
 
   /**
-   * Returns the frequency registered data from a specified column.
+   * Returns the distinct values for the given column coupled with the number
+   * of occurrences for that value. The tuples are sorted descendingly on the
+   * number of occurrences. The first element is the column header together
+   * with the total number of occurrences for all values.
    */
   def register(c: Int): Vector[(String, Int)] = ???
 }
@@ -43,22 +40,17 @@ case class Table(
 object Table {
 
   /**
-   * Reads column separated text data from either
-   * a file or a URL into a Table.
-   * 
+   * Reads column separated text data from either a file or a URL into a Table.
+   *
    * @param uri The location of the data.
    * @param sep The character that separates the columns.
    */
   def fromFile(uri: String, sep: String): Table = ???
 
-  /**
-   * Writes a text formatted Table to disk.
-   */
+  /** Writes a text formatted Table to disk. */
   def toFile(path: String, table: Table): Unit = ???
 
-  /**
-   * To test Table
-   */
+  /** To test Table */
   def main(args: Array[String]): Unit = {
     val table = Table.fromFile("src/main/resources/favorit.csv", ",")
     val tablefs = table.filter(4, Vector("Linux")).sort(6)
