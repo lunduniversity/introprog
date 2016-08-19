a4a24be41e641e6233c93c9885246f1ebb7631c1
tejp <cyberkygen@gmail.com>
2016-08-19 02:19:09 +0200

    change to scalainputlisting for Main

--- workspace/w10_survey/src/main/scala/stats/Main.scala
+++ workspace/w10_survey/src/main/scala/stats/Main.scala
@@ -4,13 +4,14 @@ import scala.util.Try
 
 object Main {
 
-  val usage = s"Usage: <uri> <separator> [Commands]\n\n${Command.list}"
+  val usage =
+    s"Usage: <uri> <separator> [Commands]\n\n${Command.list}"
 
   def main(args: Array[String]): Unit = args.toVector match {
 
     case uri +: sep +: rest if rest != Vector() => {
       Try {
-        println(s"""Loading "$uri" with separator "$sep" ...\n""")
+        println(s"""Loading "$uri" "$sep" to table ...\n""")
         val t = Table.fromFile(uri, sep)
         println(s"Done. Size: ${t.dim._1}x${t.dim._2}.\n")
         val commands = Command.parseAll(rest)
