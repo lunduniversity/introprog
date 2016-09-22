e7a5e5d11cfee87de1b9486d170c30b5294418d5
bjornregnell <bjorn.regnell@cs.lth.se>
2016-09-22 15:55:26 +0200

    update seqCopy example

--- compendium/examples/workspace/w05-seqalg/src/seqCopy.scala
+++ compendium/examples/workspace/w05-seqalg/src/seqCopy.scala
@@ -1,7 +1,21 @@
-
-
 object seqCopy {
+  def arrayCopy(xs: Array[Int]): Array[Int] = {
+    val result = new Array[Int](xs.size)
+    var i = 0
+    while (i < xs.size) {
+      result(i) = xs(i)
+      i += 1
+    }
+    result
+  }
+
+  def test(copy: Array[Int] => Array[Int]): String = {
+    val xs = Array(1,2,3,4,42)
+    val ys = copy(xs)
+    if (xs.sameElements(ys)) "OK!" else "ERROR!"
+  }
+
   def main(args: Array[String]): Unit = {
-    println("hej")
+    println(test(arrayCopy))
   }
 }
