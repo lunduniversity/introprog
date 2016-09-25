042212abb858e471a2ffa5dbc9f6e9524cff85f3
bjornregnell <bjorn.regnell@cs.lth.se>
2016-09-25 16:39:26 +0200

    update slides week 05 add Scanner etc

--- compendium/examples/workspace/w05-seqalg/src/NanananananananaNanananananananaBatman.scala
+++ compendium/examples/workspace/w05-seqalg/src/NanananananananaNanananananananaBatman.scala
@@ -1,36 +1,34 @@
 object NanananananananaNanananananananaBatman {
-  import Timer.measure
-
   val na = "NanananananananaNanananananananaBatman"
 
-  def batmanImmutableTime(n: Int): (Long, String) = measure {
+  def batmanImmutable(n: Int): (Long, String) = Timer.measure {
     var result: String = na  // Strings are immutable
     for (i <- 2 to n) {
-      result = result + na   // Creates a new String
+      result = result + na   // Allocates a new String for each append
     }
     result  // return da String
   }
 
-  def batmanMutableTime(n: Int): (Long, String) = measure {
+  def batmanMutable(n: Int): (Long, String) = Timer.measure {
     var sb = new StringBuilder(na)  // StringBuilder is mutable
     for (i <- 2 to n) {
-        sb.append(na)     // append ***mutates*** StringBuilder
+        sb.append(na)     // append ***mutates*** the instance in place
     }
-    sb.toString       // convert to immutable String and return
+    sb.toString           // convert to immutable String and return
   }
 
   def main(args: Array[String]): Unit = {
-    val warmupJVM = (batmanMutableTime(100), batmanImmutableTime(100))
+    val warmupJVM = (batmanMutable(100), batmanImmutable(100))
     println("Warmup:\n" + warmupJVM._1 + "\n" + warmupJVM._2 + "\n\n")
 
     var n = 1
-    val max = math.pow(2, 10)
+    val max = math.pow(2, 9)
     println(s"max = $max")
     println("#APPEND\tIMMUTABLE\tMUTABLE")
     while (n <= max) {
       print(n)
-      val immutable = batmanImmutableTime(n)
-      val mutable = batmanMutableTime(n)
+      val immutable = batmanImmutable(n)
+      val mutable = batmanMutable(n)
       print("\t\t" + immutable._1 + " ms")
       println("\t\t"  + mutable._1   + " ms")
       n *= 2
