43d6b967305bde2cd01998304e71a2e75ee70697
bjornregnell <bjorn.regnell@cs.lth.se>
2016-09-30 18:27:09 +0200

    add java examples on classes for slides week06

---
+++ compendium/examples/JComplexTest.java
@@ -0,0 +1,7 @@
+public class JComplexTest {
+    public static void main(String[] args){
+        JComplex jc1 = new JComplex(3,5);
+        JComplex jc2 = new JComplex(1,2);
+        System.out.println(jc1.add(jc2));
+    }
+}
