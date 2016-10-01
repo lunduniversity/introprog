42aea0dd16533eee9b9a404f74b1d41f9d958caa
bjornregnell <bjorn.regnell@cs.lth.se>
2016-10-01 17:22:09 +0200

    update slides week06 add alternative constructors etc

--- compendium/examples/JComplexTest.java
+++ compendium/examples/JComplexTest.java
@@ -1,6 +1,8 @@
 public class JComplexTest {
     public static void main(String[] args){
-        JComplex jc1 = new JComplex(3,5);
+        JComplex jc1 = new JComplex(3,4);
+        String polar = "(" + jc1.getR() + ", " + jc1.getFi() + ")";
+        System.out.println("Polär form: " + polar);
         JComplex jc2 = new JComplex(1,2);
         System.out.println(jc1.add(jc2));
     }
