254b46a5f1dc2c7a2aef7bdfbcea6d128ddfcd63
bjornregnell <bjorn.regnell@cs.lth.se>
2016-10-08 20:53:05 +0200

    add examples workspace week07

---
+++ compendium/examples/workspace/w07-inherit/src/vegoTest.scala
@@ -0,0 +1,12 @@
+object vegoTest {
+  def main(args: Array[String]): Unit = {
+    val g = new Gurka(200)
+    val t = new Tomat(70)
+    println(g)
+    println(t)
+    val gs = Vector(g, t)
+    println(gs)
+    gs.foreach(_.skala)
+    gs.foreach(println)
+  }
+}
