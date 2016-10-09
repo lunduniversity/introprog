57fbd38f0462b8d59189d88781005f25b8995e6d
bjornregnell <bjorn.regnell@cs.lth.se>
2016-10-09 15:20:33 +0200

    update refactor examples workspace w07 vego

---
+++ compendium/examples/workspace/w07-inherit/src/vego1.scala
@@ -0,0 +1,28 @@
+object exempelVego1 {
+
+  trait Grönsak {
+    def skala(): Unit
+    var vikt: Double
+    val namn: String
+    var ärSkalad: Boolean = false
+    override def toString = s"$namn ${if (!ärSkalad) "o" else ""}skalad $vikt g"
+  }
+
+  class Gurka(var vikt: Double) extends Grönsak {
+    val namn = "gurka"
+    def skala(): Unit = if (!ärSkalad) {
+      println("Gurkan skalas med skalare.")
+      vikt = 0.99 * vikt
+      ärSkalad = true
+    }
+  }
+
+  class Tomat(var vikt: Double) extends Grönsak {
+    val namn = "tomat"
+    def skala(): Unit = if (!ärSkalad) {
+      println("Tomaten skalas genom skållning.")
+      vikt = 0.99 * vikt
+      ärSkalad = true
+    }
+  }
+}
