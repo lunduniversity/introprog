f1db8e4a2876bff176618c991cf43073c27730f6
bjornregnell <bjorn.regnell@cs.lth.se>
2016-10-10 18:43:20 +0200

    update ordering of trait in w07 CanDraw

--- compendium/examples/workspace/w07-inherit/src/draw.scala
+++ compendium/examples/workspace/w07-inherit/src/draw.scala
@@ -1,9 +1,11 @@
+trait CanDraw {
+  def draw(dw: DrawingWindow): Unit
+}
+
 trait DrawingWindow {
   def penTo(pt: (Double, Double)): Unit
   def drawTo(pt: (Double, Double)): Unit
 }
 
-trait CanDraw {
-  def draw(dw: DrawingWindow): Unit
-}
+
 
