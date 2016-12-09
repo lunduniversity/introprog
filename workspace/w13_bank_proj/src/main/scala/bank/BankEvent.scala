762b58b3d3e5bc75de68a695c925f947f0d53798
bjornregnell <bjorn.regnell@cs.lth.se>
2016-12-09 10:57:12 +0100

    add BankEvent implementation to workspace

---
+++ workspace/w13_bank_proj/src/main/scala/bank/BankEvent.scala
@@ -0,0 +1,19 @@
+sealed trait BankEvent {   
+  def logString: String
+}
+
+case class Deposit(account: Int, amount:  BigInt) extends BankEvent {
+  def logString: String = s"D $account $amount"
+}
+case class Withdraw(account: Int, amount: BigInt) extends BankEvent {
+  def logString: String = s"W $account $amount"
+}
+case class Transfer(accFrom: Int, accTo: Int, amount: BigInt) extends BankEvent {
+  def logString: String = s"T $accFrom $accTo $amount"
+}
+case class NewAccount(id: Int, name: String) extends BankEvent {
+  def logString: String = s"N $id $name"
+}
+case class DeletedAccount(account: Int) extends BankEvent {
+  def logString: String = s"E $account"
+}
