0d5ba1597129eb5a9795a2ac268347a8e6bb4e65
bjornregnell <bjorn.regnell@cs.lth.se>
2016-12-14 09:47:25 +0100

    update/add specs for timeplanner exam

--- compendium/examples/exam/re-impl-java-exams/timeplanner-160824/timeplanner-spec2.scala
+++ compendium/examples/exam/re-impl-java-exams/timeplanner-160824/timeplanner-spec2.scala
@@ -1,20 +1,22 @@
-// En Java-orienterad Scala-specifikation av extentalösningen 2016-08-24
+// En lite mer "Scala-idiomatisk" specifikation av extentaproblemet 2016-08-24
 
-class WorkPeriod(private val task: String, private val hour: Int, length: Int) {
+import scala.collection.mutable.ArrayBuffer // import krävs ej på tentan
+ 
+case class WorkPeriod(task: String, hour: Int, length: Int){
   def collidesWith(wp: WorkPeriod): Boolean = ???
     
-  def compareTo(wp: WorkPeriod): Int =  ???
+  def compareTo(wp: WorkPeriod): Int = ???
   
   override def toString = ???
 }
 
 class WorkPeriodList {
-  def add(task: String, hour: Int, length: Int): Unit =  ???
+  def add(task: String, hour: Int, length: Int): Unit = ???
   
-  def toSortedArray: Array[WorkPeriod] = ???
+  def toSortedVector: Vector[WorkPeriod] = ???
 }
 
-class Worker(val name: String, private val times: Array[WorkPeriod]) {
+class Worker(val name: String, val times: Vector[WorkPeriod]) {
   def schedule(nbr: Int): Unit = ???  
   
   def isScheduled(nbr: Int): Boolean = ???
@@ -22,15 +24,15 @@ class Worker(val name: String, private val times: Array[WorkPeriod]) {
   def canWork(nbr: Int): Boolean = ???
 }
 
-class TimePlanner(private val times: Array[WorkPeriod]){ 
+class TimePlanner(private val times: Vector[WorkPeriod]){ 
   def addWorker(name: String): Boolean = ???
   
   def scheduleWorker(name: String, nbr: Int): Unit = ???
   
-  def availableTimes: ArrayBuffer[WorkPeriod] = ???
+  def availableTimes: Vector[WorkPeriod] = ???
 }
 
-object Test { //testprogram, ingår ej i tentan men kan hjälpa att fundera på
+object Test { 
   def main(args: Array[String]) = {
     val wpl = new WorkPeriodList
     wpl.add("Städa toaletter", 17, 2)  // 0
@@ -40,14 +42,14 @@ object Test { //testprogram, ingår ej i tentan men kan hjälpa att fundera på
     wpl.add("Städa toaletter", 19, 2)  // 4
     wpl.add("Vakta entren", 20, 2)     // 5
     wpl.add("Plocka skräp", 22, 1)     // 6
-    val times = wpl.toSortedArray
+    val times = wpl.toSortedVector
 
-    println(times.mkString("ARBETSPASS:\n  ","\n ","\n"))
+    println(times.mkString("ARBETSPASS:\n ","\n  ","\n"))
     val planner = new TimePlanner(times)
 
     def showAvailable(head: String) = 
       println(planner.availableTimes.mkString(s"$head:\n ","\n ", "\n"))
-      
+
     showAvailable("LEDIGA före allokering:")
 
     val allocation = Map(
@@ -56,13 +58,11 @@ object Test { //testprogram, ingår ej i tentan men kan hjälpa att fundera på
       "Joey" -> Vector(2), 
       "Kim" -> Vector(5)
     )
-
     allocation.foreach { tuple2 =>
       val (name, nbrs) = tuple2
       planner.addWorker(name)
       nbrs.foreach(nbr => planner.scheduleWorker(name, nbr))
     }
- 
     showAvailable("LEDIGA efter allokering:")
   }
 }
