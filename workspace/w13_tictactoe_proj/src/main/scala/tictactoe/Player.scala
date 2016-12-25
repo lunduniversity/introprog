4f24d6aa9611bc87c40f741e5c104468a31201af
Jos Rosenqvist <jos.rosenqvist.3812@student.lu.se>
2016-12-25 14:29:43 +0100

    tictactoe: Text cleanup

--- workspace/w13_tictactoe_proj/src/main/scala/tictactoe/Player.scala
+++ workspace/w13_tictactoe_proj/src/main/scala/tictactoe/Player.scala
@@ -1,15 +1,16 @@
 abstract class Player(name: String) {
 	
 	/**
-	 * abstract method, should not be implemented here, but requierd by extentions of Player.
-	 * returns an int p in the interval [0,8] where game(p) == 0,
+	 * Abstract method. Should not be implemented here,
+	 * but required by subtypes of Player.
+	 * Returns an int p in the interval [0,8] where game(p) == 0,
 	 * the index of the move this player does. 
 	 */
 	def move(game: Array[Int], depth: Int): Int;
 	
-	// returns true if there exists a row, collumn or diagonal where all the elements are equal to who.
-	def gameWon(game: Array[Int],who:Int): Boolean =  ???
+	// returns true if there exists a row, column or diagonal where all the elements are equal to who.
+	def gameWon(game: Array[Int], who: Int): Boolean = ???
 	
-	// returns a String with information about the player, the name, for example.
+	// Returns a String with information about the player, for example the name.
 	override def toString(): String = ???
 }
