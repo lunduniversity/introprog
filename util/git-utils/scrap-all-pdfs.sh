d2460ea915c42a8838b26ce109c4e92ab196965d
bjornregnell <bjorn.regnell@cs.lth.se>
2017-03-22 18:03:15 +0100

    add more dirs with pdfs

--- util/git-utils/scrap-all-pdfs.sh
+++ util/git-utils/scrap-all-pdfs.sh
@@ -1,9 +1,9 @@
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch compendium/compendium.pdf' --prune-empty --tag-name-filter cat -- --all
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch compendium/exercises.pdf' --prune-empty --tag-name-filter cat -- --all
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch compendium/solutions.pdf' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch compendium/*.pdf' --prune-empty --tag-name-filter cat -- --all
 git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch slides/*.pdf' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch quickref/*.pdf' --prune-empty --tag-name-filter cat -- --all
 git push origin --force --all
 git push origin --force --tags
+git pull
 git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
 git reflog expire --expire=now --all
 git gc --prune=now
