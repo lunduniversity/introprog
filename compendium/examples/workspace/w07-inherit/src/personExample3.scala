217d73c1c01ca252cfd943fa497f9f7d68d299a8
bjornregnell <bjorn.regnell@cs.lth.se>
2017-05-09 21:18:31 +0200

    minor fixes in compendium1

--- compendium/examples/workspace/w07-inherit/src/personExample3.scala
+++ compendium/examples/workspace/w07-inherit/src/personExample3.scala
@@ -1,23 +1,23 @@
 object personExample3 {
 
-  trait Person { val namn: String }
-
+  trait Person     { val namn:        String }
   trait Akademiker { val universitet: String }
-
-  trait Examinerad { val titel: String }
+  trait Examinerad { val titel:       String }
 
   class Student(val namn: String,
                 val universitet: String,
-                val program: String) extends Person with Akademiker
+                val program: String)
+                extends Person with Akademiker
 
   class Forskare(val namn: String,
                  val universitet: String,
-                 val titel: String) extends Person with Akademiker with Examinerad
+                 val titel: String)
+                 extends Person with Akademiker with Examinerad
 
   def main(args: Array[String]): Unit = {
     var p: Person = new Forskare("Robin Smith", "Lund", "Professor Dr")
     if (p.isInstanceOf[Akademiker]) println(p.namn + " är akademiker")
     p = new Student("Kim Robinson", "Lund", "Data")
     if (p.isInstanceOf[Student]) println(p.asInstanceOf[Student].program)
-  }
-}
+  }  // Ovan görs hellre med match,
+}    // undvik om det går att använda isInstanceOf och asInstanceOf.
