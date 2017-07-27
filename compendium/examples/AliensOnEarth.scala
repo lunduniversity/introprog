5152091f5ce6db168672d35db2a60064a321f033
bjornregnell <bjorn.regnell@cs.lth.se>
2017-07-27 22:14:00 +0200

    update more work on exercise w03 funtions

--- compendium/examples/AliensOnEarth.scala
+++ compendium/examples/AliensOnEarth.scala
@@ -1,35 +1,34 @@
 object AliensOnEarth {
   def userSelect(msg: String, options: Vector[String]): String = {
     options.indices.foreach(i => println(i + ": " + options(i)))
-    val selected = scala.io.StdIn.readLine(msg).toInt
+    val selected = io.StdIn.readLine(msg).toInt
     options(selected)
   }
 
   def isUserOk(msg: String): Boolean =
-    scala.io.StdIn.readLine(s"$msg (Y/n)").toLowerCase.startsWith("y")
+    io.StdIn.readLine(s"$msg (Y/n)").toLowerCase.startsWith("y")
 
   def randomSelect(options: Vector[String]): String = {
-    val selected = scala.util.Random.nextInt(options.size)
+    val selected = util.Random.nextInt(options.size)
     options(selected)
   }
 
   def play(alien: String, maxPoints: Int = 1000): Int = {
-    val objects = Vector("penguin", "window", "apple")
-    val correct =
-      if (math.random < 0.8) objects(0) else randomSelect(objects)
-    val cheatCode = (objects.indexOf(correct) + 1) * math.Pi
+    val os = Vector("penguin", "window", "apple")
+    val correct = if (math.random < 0.5) os(0) else randomSelect(os)
+    val cheatCode = (os.indexOf(correct) + 1) * math.Pi
     println(s"""Hello $alien!
                |You are an alien on Earth.
                |Your encrypted password is $cheatCode.
                |You see three strange Earth objects.""".stripMargin)
-    val choice = userSelect(s"$alien wants? ", objects)
+    val choice = userSelect(s"$alien wants? ", os)
     if (choice == correct) maxPoints else 0
   }
 
   def main(args: Array[String]): Unit =
-   try {
-      val name = if (args.size > 0) args(0) else "Master of Universe"
-      val points = play(alien = name, maxPoints = 1000)
+    try {
+      val name = if (args.size > 0) args(0) else "Captain Zoom"
+      val points = play(alien = name)
       if (points > 0) println(s"Congratulations $name! :)")
       println(s"You got $points points.")
     } catch { case e: Exception =>
