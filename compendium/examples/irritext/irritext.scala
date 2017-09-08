3815b6b474cd550fc3b5592d6626eff39f732fe7
bjornregnell <bjorn.regnell@cs.lth.se>
2017-09-08 20:33:01 +0200

    update irritext example improve whitespace and naming

--- compendium/examples/irritext/irritext.scala
+++ compendium/examples/irritext/irritext.scala
@@ -1,32 +1,39 @@
 object irritext {
   import scala.io.StdIn.readLine
-  
-  def youAreDead: Unit = println("DÖÖÖÖÖÖD!")
-  
-  def youMadeIt(points: Int): Unit = 
-    println(s"GRATTIS DU FICK $points POÄNG!")
-  
-  def main(args: Array[String]): Unit = {
-    println("Välkommen till ett lagom irriterande textspel!")
-    println("Du står framför en vägg med två dörrar.")
-    println("Du hör morrande och rytande.")
-    val msg = "Vilken dörr? V=vänster H=Höger\n"
-    var dead = false
-    var ready = false
-    var points = 0
-    while (!dead && !ready) {
-      val door = readLine(msg)
+
+  def printDead(): Unit = println("Du är nu DÖÖÖÖÖÖD! Tack och hej :(")
+  def printSolved(points: Int): Unit = println(s"GRATTIS DU FICK $points POÄNG!")
+
+  def printWelcomeMessage(): Unit = println("""
+    |Välkommen till ett lagom irriterande textspel (om du läst koden)!\n
+    |Du står framför en fuktdrypande vägg med två tunga dörrar.
+    |Du hör morrande och rytande och det blåser en iskall vind.
+  """.stripMargin)
+
+  def doorChoice(): String = readLine("Vilken dörr väljer du? V=vänster H=Höger\n")
+
+  var isDead = false
+  var isSolved = false
+  var points = 0
+
+  def play(): Unit = {
+    while (!isDead && !isSolved) {
+      val door: String = doorChoice()
       if (door.toLowerCase.startsWith("v")) {
         println("Du klarade det!")
-        ready = true
+        isSolved = true
       } else if (door.toLowerCase.startsWith("h")) {
         println("Du blev uppäten av lejonet och dog...")
-        dead = true
+        isDead = true
       } else {
         println("FEL! Den dörren finns inte. Men försöka duger.")
         points += 10
       }
     }
-    if (dead) youAreDead else youMadeIt(points)
+  }
+
+  def main(args: Array[String]): Unit = {
+    play()
+    if (isDead) printDead else printSolved(points)
   }
 }
