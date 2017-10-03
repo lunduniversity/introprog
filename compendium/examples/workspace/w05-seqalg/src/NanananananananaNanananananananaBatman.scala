57e2dc7e9da5394dfc21bd9268752b3bb406b40c
bjornregnell <bjorn.regnell@cs.lth.se>
2017-10-03 17:45:53 +0200

    update slides w06 seq including examples

--- compendium/examples/workspace/w05-seqalg/src/NanananananananaNanananananananaBatman.scala
+++ compendium/examples/workspace/w05-seqalg/src/NanananananananaNanananananananaBatman.scala
@@ -1,38 +1,33 @@
 object NanananananananaNanananananananaBatman {
   val na = "NanananananananaNanananananananaBatman"
+  val nAppendsMax = math.pow(2, 15).toLong
 
   def batmanImmutable(n: Int): (Long, String) = Timer.measure {
-    var result: String = na  // Strings are immutable
-    for (i <- 2 to n) {
-      result = result + na   // Allocates a new String for each append
-    }
+    var result: String = na  //strings are immutable
+    for (i <- 2 to n) result = result + na //allocates new String for each append
     result  // return da String
   }
 
   def batmanMutable(n: Int): (Long, String) = Timer.measure {
-    var sb = new StringBuilder(na)  // StringBuilder is mutable
-    for (i <- 2 to n) {
-        sb.append(na)     // append ***mutates*** the instance in place
-    }
-    sb.toString           // convert to immutable String and return
+    var sb = new StringBuilder(na)  //StringBuilder is mutable
+    for (i <- 2 to n) sb.append(na) //append ***mutates*** the instance in place
+    sb.toString                     //convert to immutable String and return
   }
 
   def main(args: Array[String]): Unit = {
     val warmupJVM = (batmanMutable(100), batmanImmutable(100))
     println("Warmup:\n" + warmupJVM._1 + "\n" + warmupJVM._2 + "\n\n")
-
     var n = 1
-    val max = math.pow(2, 9)
-    println(s"max = $max")
-    println("#APPEND\tIMMUTABLE\tMUTABLE")
-    while (n <= max) {
+    println(s"Max number of appends = $nAppendsMax")
+    println("#APPEND\tMUTABLE\tIMMUTABLE")
+    while (n <= nAppendsMax) {
       print(n)
-      val immutable = batmanImmutable(n)
       val mutable = batmanMutable(n)
-      print("\t\t" + immutable._1 + " ms")
-      println("\t\t"  + mutable._1   + " ms")
+      print("\t"  + mutable._1   + " ms")
+      val immutable = batmanImmutable(n)
+      println("\t" + immutable._1 + " ms")
       n *= 2
     }
-    println("BATMAN READY!!!!!!!!!!")
+    println("\nBATMAN READY!!!!!!!!!!")
   }
 }
