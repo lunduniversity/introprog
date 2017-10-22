4c9d9f5bd45dfb72794f6b72ba4ecacc26ecbd40
bjornregnell <bjorn.regnell@cs.lth.se>
2017-10-22 14:56:09 +0200

    update lab w10_music ready for release

--- workspace/w10_music/src/main/scala/music/Main.scala
+++ workspace/w10_music/src/main/scala/music/Main.scala
@@ -1,18 +1,8 @@
 package music
 
-// https://maven2repo.com/jline/jline/2.14.4/jar
-//  wget -O lib/jline-2.14.4.jar http://search.maven.org/remotecontent?filepath=jline/jline/2.14.4/jline-2.14.4.jar
-
-// https://stackoverflow.com/questions/34360822/midi-device-not-showing-up-for-midisystem-getmidideviceinfo
-
-// https://stackoverflow.com/questions/18676712/java-sound-devices-found-when-run-in-intellij-but-not-in-sbt
-
-// http://www.scala-sbt.org/1.x/docs/Forking.html
-
 object Main {
   val (helloMsg, exitMsg) = ("*** Welcome to music!", "Goodbye music!")
-  val console = new jline.console.ConsoleReader
-  def readLine(): String = console.readLine("music> ")
+  def readLine(): String = scala.io.StdIn.readLine("music> ")
 
   def main(args: Array[String]): Unit = {
     println(helloMsg)
