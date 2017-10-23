fdec2065216c0180ddd16284d9b53f14de7022ce
bjornregnell <bjorn.regnell@cs.lth.se>
2017-10-23 22:56:38 +0200

    bulk update to prepare for release v2.2-RC1

--- compendium/examples/numbers.scala
+++ compendium/examples/numbers.scala
@@ -1,10 +1,10 @@
 package numbers
 
 trait Number {
-  def reduce: Number = this  
-  def isZero: Boolean 
-  def isOne: Boolean 
-  def +(that: Number): Number = ???  
+  def reduce: Number = this
+  def isZero: Boolean
+  def isOne: Boolean
+  def +(that: Number): Number = ???
 }
 
 object Number {
@@ -20,8 +20,8 @@ object Number {
 trait AbstractComplex extends Number {
   def re: AbstractReal
   def im: AbstractReal
-  def abs = Real(math.hypot(re.decimal.toDouble,im.decimal.toDouble)) 
-  def fi =  Real(math.atan2(im.decimal.toDouble, re.decimal.toDouble)) 
+  def abs = Real(math.hypot(re.decimal.toDouble,im.decimal.toDouble))
+  def fi =  Real(math.atan2(im.decimal.toDouble, re.decimal.toDouble))
   override def isZero: Boolean = re.decimal == 0 && im.decimal == 0
   override def isOne: Boolean = abs.decimal == 1.0
   override def reduce: AbstractComplex = if (im.decimal == 0) re.reduce else this
@@ -34,17 +34,16 @@ object Complex {
 }
 
 case class Polar(
-    override val abs: Real, 
+    override val abs: Real,
     override val fi: Real
   ) extends AbstractComplex {
-  override def re = Real(abs.decimal.toDouble * math.cos(fi.decimal.toDouble))  
+  override def re = Real(abs.decimal.toDouble * math.cos(fi.decimal.toDouble))
   override def im = Real(abs.decimal.toDouble * math.sin(fi.decimal.toDouble))
 }
 
 object Polar {
   def apply(abs: BigDecimal, fi: BigDecimal) = new Polar(Real(abs), Real(fi))
 }
- 
 
 trait AbstractReal extends AbstractComplex {
   def decimal: BigDecimal
@@ -52,18 +51,18 @@ trait AbstractReal extends AbstractComplex {
   override def isOne = decimal == 1
   override def re = this
   override def im = Number.Zero
-  override def reduce:  AbstractReal = 
+  override def reduce:  AbstractReal =
     if (decimal == 0) Number.Zero else if (decimal == 1) Number.One else this
 }
 
-case class Real(decimal: BigDecimal) extends AbstractReal 
+case class Real(decimal: BigDecimal) extends AbstractReal
 
 trait AbstractRational extends AbstractReal {
   def numerator: AbstractInteger
-  def denominator: AbstractInteger 
+  def denominator: AbstractInteger
   override def decimal = BigDecimal(numerator.integ)
   override def isOne = numerator.integ == denominator.integ
-  override def reduce: AbstractRational = 
+  override def reduce: AbstractRational =
     if (denominator.isOne) numerator.reduce else this // should use GCD
 }
 
@@ -78,17 +77,17 @@ object Frac {
 trait AbstractInteger extends AbstractRational {
   def integ: BigInt
   override def numerator = this
-  override def denominator = Number.One 
+  override def denominator = Number.One
   override def isZero = integ == 0
   override def isOne = integ == 1
   override def decimal: BigDecimal = BigDecimal(integ)
-  override def reduce: AbstractInteger = 
+  override def reduce: AbstractInteger =
     if (isZero) Number.Zero else if (isOne) Number.One else this
 }
 
-case class Integ(integ: BigInt) extends AbstractInteger 
+case class Integ(integ: BigInt) extends AbstractInteger
 
-trait AbstractNatural extends AbstractInteger 
+trait AbstractNatural extends AbstractInteger
 
 case class Nat(integ: BigInt) extends AbstractNatural{
   require(integ >= 0, "natural numnbers must be non-negative")
--- quiz/QuizData.scala
+++ quiz/QuizData.scala
@@ -424,6 +424,27 @@ object QuizData {  // to generate tables for a concept connection quizes in late
       "" -> ""
     ).filter(_._1.trim.nonEmpty),
 
+    QuizID("quiz-w09-concepts") -> Vector(  //sequences
+      "bastyp"             -> "den mest generella typen i en arvshierarki",
+      "sypertyp"           -> "en typ som är mer generell",
+      "subtyp"             -> "en typ som är mer specifik",
+      "körtidstyp"         -> "kan vara mer specifik än den statiska typen",
+      "dynamisk bindning"  -> "körtidstypen avgör vilken metod som körs",
+      "plymorfism"         -> "kan ha många former, t.ex. en av flera subtyper",
+      "trait"              -> "abstrakt klass, kan mixas in, kan ej ha parametrar",
+      "inmixning"          -> "klass får nya egenskaper från trait",
+      "överskuggad medlem" -> "medlem i subtyp ersätter medlem i supertyp",
+      "anonym klass"       -> "den mest generella typen i en arvshierarki",
+      "skyddad medlem"     -> "är endast synlig i subtyper",
+      "abstrakt medlem"    -> "saknar implementation",
+      "abstrakt klass"     -> "kan ej instansieras",
+      "referenstyp"        -> "ej värdetyp, har supertypen \\code|AnyRef|",
+      "förseglad typ"      -> "subtypning utanför denna kodfil är förhindrad",
+      "värdetyp"           -> "minneslagring kan optimeras, har supertypen, \\code|AnyVal|",
+      "" -> ""
+    ).filter(_._1.trim.nonEmpty),
+
+
     QuizID("") -> Vector()
   ).filter(_._1.name.trim.nonEmpty)
 }
