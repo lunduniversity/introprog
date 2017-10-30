ccf496e125f6c46dd14a8b7becffed936ff7b9dc
bjornregnell <bjorn.regnell@cs.lth.se>
2017-10-30 20:32:34 +0100

    bump scala version in package.sh target dir

--- package.sh
+++ package.sh
@@ -1,5 +1,4 @@
 sbt eclipse
 zip -9 -r lib/workspace.zip -x=*target* -x=*build.sbt -x=*.class -x="workspace/project/*" workspace
 sbt cslib/package
-cp workspace/cslib/target/scala-2.11/cslib*.jar lib/cslib.jar
-
+cp workspace/cslib/target/scala-2.12/cslib*.jar lib/cslib.jar
