b3e2cceb3bfd0e38b029b946044b4bd70cf88496
bjornregnell <bjorn.regnell@cs.lth.se>
2017-10-30 19:50:00 +0100

    fix Main in lab w09 turtlerace

--- workspace/w09_turtlerace_team/src/main/scala/turtlerace/Main.scala
+++ workspace/w09_turtlerace_team/src/main/scala/turtlerace/Main.scala
@@ -4,7 +4,8 @@ object Main {
   def main(args : Array[String]): Unit = {
     println("TODO: ColorTurtle, RaceTurtle, TurtleRace")
     println("TODO: subtypes of RaceTurtle with different properties ")
-    val raceWindow = new RaceWindow()
-    TurtleRace.race(Seq(), raceWindow, "TODO TurtleRace")
+    val rw = new RaceWindow()
+    val tr = new TurtleRace()
+    tr.race(Seq(), rw, "TODO TurtleRace")
   }
 }
