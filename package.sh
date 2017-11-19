5762169f41e83cc2907ece93cfe023cf6577c147
bjornregnell <bjorn.regnell@cs.lth.se>
2017-11-19 14:19:08 +0100

    repackage after merge

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 sbt eclipse
 zip -9 -r lib/workspace.zip -x=*target* -x=*build.sbt -x=*.class -x="workspace/project/*" workspace
 sbt cslib/package
-cp workspace/cslib/target/scala-2.12/cslib*.jar lib/cslib.jar
+cp workspace/cslib/target/scala-2.12/cslib_2.12-2017.2.jar lib/cslib.jar
+# for file in workspace/cslib/target/scala-2.12/cslib*.jar; do echo cp "$file" "lib/cslib.jar";done
