d3d85321c25e9dba52044770a0cec824925dd91d
Jos Rosenqvist <jos.rosenqvist.3812@student.lu.se>
2018-09-21 11:41:10 +0200

    shuffle: Add a testTally function
    Fixes #301.

--- workspace/w06_shuffle/src/main/scala/cards/TestHand.scala
+++ workspace/w06_shuffle/src/main/scala/cards/TestHand.scala
@@ -14,7 +14,32 @@ object TestHand {
     println(s"nameSwedish:     ${Hand.Category.Name.swedish(h.category)}")
   }
 
+  def testTally(): Unit = {
+    def runTest(hand: Hand, expected: Vector[Int]): Unit = {
+      val actual = hand.tally
+      if (expected == actual)
+        println(s"OK: $hand gives $actual")
+      else
+        println(s"Test failed: $hand gives $actual but should give $expected")
+    }
+
+    println("\n*** TestHand.testTally:")
+
+    runTest(Hand(Card(1, 1)),
+            Vector(0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+
+    runTest(Hand(Card(4, 2), Card(4, 1), Card(5, 3), Card(4, 3), Card(11, 1)),
+            Vector(0, 0, 0, 0, 3, 1, 0, 0, 0, 0, 0, 1, 0, 0))
+
+    runTest(Hand(Card(13, 4), Card(11, 1), Card(11, 4), Card(11, 2), Card(1, 4)),
+            Vector(0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1))
+
+    runTest(Hand(),
+            Vector(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+  }
+
   def main(args: Array[String]): Unit = {
     testCategorize()
+    testTally()
   }
 }
