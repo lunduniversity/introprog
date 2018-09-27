b4bdce34ba1e0dc56ce697a24d3b728ea5912bbe
bjornregnell <bjorn.regnell@cs.lth.se>
2018-09-27 17:13:57 +0200

    add removal of workspace.zip so not appending files to uncleared zip

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 sbt eclipse
+rm -i lib/workspace.zip
 zip -9 -r lib/workspace.zip -x=*target* -x=*build.sbt -x=*.class -x="workspace/project/*" workspace
 sbt cslib/package
 cp workspace/cslib/target/scala-2.12/cslib_2.12-2017.2.jar lib/cslib.jar
