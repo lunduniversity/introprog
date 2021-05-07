c2d20a1fa8643fd042914d1d662e7e581269d590
bjornregnell <bjorn.regnell@cs.lth.se>
2021-05-07 16:15:30 +0200

    migrate workspace w04 blockmole

--- workspace/w04_blockmole/build.sbt
+++ workspace/w04_blockmole/build.sbt
@@ -1,4 +1,4 @@
-scalaVersion := "2.13.5"
-libraryDependencies += "se.lth.cs" %% "introprog" % "1.1.4"
+scalaVersion := "3.0.0-RC3"
+libraryDependencies += "se.lth.cs" % "introprog_2.13" % "1.1.4"
 scalacOptions := Seq("-unchecked", "-deprecation")
 
--- workspace/w04_blockmole/hello-window.scala
+++ workspace/w04_blockmole/hello-window.scala
@@ -1,10 +1,9 @@
 import introprog.PixelWindow
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("HELLO main method! åäö ÅÄÖ")
-    val w = new PixelWindow(width = 800, height = 600, title = "Hello Window!")
-    w.drawText(x = 100, y = 200, text = "HELLO PIXEL WINDOW! ÅÄÖ åäö")
-    introprog.examples.TestPixelWindow.main(Array())
-  }
-}
+@main
+def run: Unit = 
+  println("HELLO main method! åäö ÅÄÖ")
+  val w = new PixelWindow(width = 800, height = 600, title = "Hello Window!")
+  w.drawText(x = 100, y = 200, text = "HELLO PIXEL WINDOW! ÅÄÖ åäö")
+  introprog.examples.TestPixelWindow.main(Array())
+
