96254f24e512de227e7fa09055dc585aaaf587a0
bjornregnell <bjorn.regnell@cs.lth.se>
2021-06-04 17:26:40 +0200

    migrate examples/lth-courses to scala3

--- compendium/examples/lth-courses/courses.scala
+++ compendium/examples/lth-courses/courses.scala
@@ -1,9 +1,8 @@
-object courses {
-  def download(): Vector[Course] = {
+object courses:
+  def download(): Vector[Course] =
     val url = "http://cs.lth.se/pgk/lthkurser"
     val lines = scala.io.Source.fromURL(url, "UTF-8").getLines.toVector
     lines.drop(1).map(s => Course.fromLine(s, '\t'))
-  }
 
   lazy val lth: Vector[Course] = download()
 
@@ -15,13 +14,10 @@ object courses {
     level:   String
   )
 
-  object Course {
-    def fromLine(line: String, separator: Char): Course = {
+  object Course:
+    def fromLine(line: String, separator: Char): Course =
       import scala.util.Try
       val xs = line.split(separator).toSeq
       def str(i: Int): String = Try(xs(i)).getOrElse("")
       def num(i: Int): Double = Try(xs(i).toDouble).getOrElse(0.0)
       Course(str(0), str(1), str(2), num(3), str(4))
-    }
-  }
-}
