1386adfede985cee946b51f28f6b79db081a4213
bjornregnell <bjorn.regnell@cs.lth.se>
2021-06-05 18:21:47 +0200

    add show sbt to version script

--- show-scala-version.sh
+++ show-scala-version.sh
@@ -1,2 +1,6 @@
+echo "  *** scala versions in build.sbt  ***"
 find . -type f -name build.sbt -exec grep -H "scalaVersion" {} \;
+
+echo " *** sbt versions in sbt.version ***"
+find . -type f -name build.properties -exec grep -H "sbt.version" {} \;
 #sed -i 's/scalaVersion := "2.13.3"/scalaVersion := "2.13.5"/g' workspace/*/build.sbt
