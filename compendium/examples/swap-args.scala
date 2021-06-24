c2d4c511ee6c25ab67ec7e8cc6be05502200cfe6
kuleshmaria <maria.kulesh@gmail.com>
2021-06-24 22:05:28 +0200

    migrate w02 minor fixes

--- compendium/examples/swap-args.scala
+++ compendium/examples/swap-args.scala
@@ -3,6 +3,6 @@ def swapFirstLastArg(args: String*): Unit =
   val xs = args.toArray
   if xs.size > 1 then
     val temp = xs(0)
-    xs(0) = xs(xs.size -1)
-    xs(xs.size -1) = temp
+    xs(0) = xs(xs.size - 1)
+    xs(xs.size - 1) = temp
   println(xs.mkString(" "))
