669da7e619cfb2608f535ead4d868c74445980c0
Lundqvist <theodor.lundqvist@gmail.com>
2021-07-25 17:09:55 +0200

    update latest scalalib

--- workspace/introprog/src/main/scala/introprog/examples/TestIO.scala
+++ workspace/introprog/src/main/scala/introprog/examples/TestIO.scala
@@ -20,7 +20,31 @@ object TestIO {
     val testResult = if (isSameContents) "SUCCESS :)" else "FAILURE :("
     assert(isSameContents, s"$highscores != $highscores2")
     println(s"$highscores == $highscores2\n$testResult")
+
+    testImageLoadAndDraw
   }
+
+  def testImageLoadAndDraw = {
+    import introprog.PixelWindow
+
+    var w = new PixelWindow(100, 100);
+    //draw text top right
+    w.drawText("test", 0, 0, java.awt.Color.red)
+    //draw entire window bottom left
+    w.drawImage(w.getImage(), 50, 50)
+    //save screenshot
+    IO.saveImage("screenshot.png", w.getImage())
+
+    //open new window with screenshot
+    new PixelWindow(100, 100).drawImage(IO.loadImage("screenshot.png"), 0, 0);
+    
+    println("if window content looks the same everything works :)")
+    
+    //delete screenshot file
+    IO.delete("screenshot.png")
+  }
+
+
 
 // for file extension choice see:
 // https://stackoverflow.com/questions/10433214/file-extension-for-a-serialized-object
