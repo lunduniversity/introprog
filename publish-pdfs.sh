7a684bd625303045c1ce95d4d04a257b184088ab
bjornregnell <bjorn.regnell@cs.lth.se>
2021-08-23 12:18:32 +0200

    add quickref*.pdf

--- publish-pdfs.sh
+++ publish-pdfs.sh
@@ -1,2 +1,2 @@
 echo "*** scp pdfs to web.cs.lth.se"
-scp compendium/*.pdf slides/*.pdf quickref/quickref.pdf plan/courseplan/courseplan.pdf $LUCATID@fileadmin.cs.lth.se:/Websites/Fileadmin/pgk/.
+scp compendium/*.pdf slides/*.pdf quickref/quickref*.pdf plan/courseplan/courseplan.pdf $LUCATID@fileadmin.cs.lth.se:/Websites/Fileadmin/pgk/.
