30522fad19d4a01721a172b046ca86e16f7366f5
bjornregnell <bjorn.regnell@cs.lth.se>
2022-04-25 16:55:49 +0200

    add kojo-lib export utility using scala-cli

---
+++ workspace/w01_kojo/kojo.scala
@@ -0,0 +1,4 @@
+//> using scala "3"
+//> using lib "kojo-lib:kojo-lib:0.1.1,url=https://github.com/lunduniversity/introprog/releases/download/kojo-0.1.1/kojo-lib-0.1.1.jar"
+
+export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*
