d1adc6d5de7c8c1f7ea1a7d566edb11c6d4b9a47
bjornregnell <bjorn.regnell@cs.lth.se>
2022-05-18 17:31:56 +0200

    fix #626 put given bank stubs in workspace

---
+++ workspace/w13_bank_proj/src/main/scala/bank/Bank.scala
@@ -0,0 +1,48 @@
+package bank
+
+/**
+ * A bank with no accounts and no history.
+ */
+class Bank():
+ /**
+   * Returns a list of every bank account in the bank.
+   * The returned list is sorted in alphabetical order based
+   * on customer name.
+   */
+  def allAccounts(): Vector[BankAccount] = ???
+
+  /**
+   * Returns the account holding the provided account number.
+   */
+  def findByNumber(accountNbr: Int): Option[BankAccount] = ???
+
+  /**
+   * Returns a list of every account belonging to
+   * the customer with the provided id.
+   */
+  def findAccountsForHolder(id: Long): Vector[BankAccount] = ???
+
+  /**
+   * Returns a list of all customers whose names match
+   * the provided name pattern.
+   */
+  def findByName(namePattern: String): Vector[Customer] = ???
+
+  /**
+   * Executes an event in the bank.
+   * Returns a string describing whether the
+   * event was successful or failed.
+   */
+  def doEvent(event: BankEvent): String = ???
+
+  /**
+   * Returns a log of all changes to the bank's state.
+   */
+  def history(): Vector[HistoryEntry] = ???
+
+  /**
+   * Resets the bank to the state it had at the provided date.
+   * Returns a string describing whether the event was
+   * successful or failed.
+   */
+  def returnToState(returnDate: Date): String = ???
---
+++ workspace/w13_bank_proj/src/main/scala/bank/Customer.scala
@@ -0,0 +1,7 @@
+package bank
+
+/**
+ * A customer of a bank with provided name and id.
+ */
+case class Customer(name: String, id: Long):
+  override def toString(): String = ???
