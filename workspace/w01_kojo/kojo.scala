e703fc734ed2587e450b5bc8e295f61257bc20f7
bjornregnell <bjorn.regnell@cs.lth.se>
2022-07-04 13:08:25 +0200

    add builtins to export of kojo-lib

--- workspace/w01_kojo/kojo.scala
+++ workspace/w01_kojo/kojo.scala
@@ -1,4 +1,4 @@
 //> using scala "3"
 //> using lib "net.kogics:kojo-lib:0.1.1,url=https://github.com/lunduniversity/introprog/releases/download/kojo-lib-0.1.1/kojo-lib-0.1.1.jar"
 
-export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*
+export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*, builtins.*
