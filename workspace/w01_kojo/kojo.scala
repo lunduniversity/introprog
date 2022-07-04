31d38817af2e670ec82e43255401cf858debeb72
bjornregnell <bjorn.regnell@cs.lth.se>
2022-07-04 13:29:27 +0200

    fix ambiguous import

--- workspace/w01_kojo/kojo.scala
+++ workspace/w01_kojo/kojo.scala
@@ -1,4 +1,5 @@
 //> using scala "3"
 //> using lib "net.kogics:kojo-lib:0.1.1,url=https://github.com/lunduniversity/introprog/releases/download/kojo-lib-0.1.1/kojo-lib-0.1.1.jar"
 
-export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*, builtins.*
+export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*
+export builtins.{TurtleAPI => _, CanvasAPI => _, *} 
