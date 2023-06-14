d8a83fe371b79596878747e161e96400693d87a0
bjornregnell <bjorn.regnell@cs.lth.se>
2023-06-14 13:42:38 +0200

    use dep instead of lib

--- workspace/w01_kojo/kojo.scala
+++ workspace/w01_kojo/kojo.scala
@@ -1,4 +1,4 @@
 //> using scala "3"
-//> using lib "net.kogics:kojo-lib:0.2.0,url=https://github.com/lunduniversity/introprog/releases/download/kojo-lib-0.2.0/kojo-lib-0.2.0.jar"
+//> using dep "net.kogics:kojo-lib:0.2.0,url=https://github.com/lunduniversity/introprog/releases/download/kojo-lib-0.2.0/kojo-lib-0.2.0.jar"
 
 export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*
