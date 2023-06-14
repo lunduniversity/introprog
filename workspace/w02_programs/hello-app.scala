75920d049787963b903d16f44115996d63de1a74
bjornregnell <bjorn.regnell@cs.lth.se>
2023-06-14 12:52:06 +0200

    update main examples in w02

--- workspace/w02_programs/hello-app.scala
+++ workspace/w02_programs/hello-app.scala
@@ -1,5 +1,7 @@
+@main def run = println("Hej Scala-app!") // en @main-funktion
+
 object Hello {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {  // en primitiv main-metod måste finnas i ett objekt
     println("Hej scala-app!")
   }
 }
--- workspace/w02_programs/hello-script.sc
+++ workspace/w02_programs/hello-script.sc
@@ -1,1 +1,5 @@
-println("hejsan script")
+
+val msg = "Hejsan Scala-script!"
+println(msg)
+println("Ett Scala-script är en fil som slutar på .sc")
+println("I ett script behövs ingen @main")
