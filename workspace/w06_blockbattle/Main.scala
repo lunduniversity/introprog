f0e7646c9c49b3a7071ef92c7a2df35c24b7e835
bjornregnell <bjorn.regnell@cs.lth.se>
2023-06-20 15:05:01 +0200

    update main w06 add todo window

--- workspace/w06_blockbattle/Main.scala
+++ workspace/w06_blockbattle/Main.scala
@@ -1,3 +1,7 @@
+package blockbattle
 object Main:
   def main(args: Array[String]): Unit =
-    println("TODO: blockbattle")
+    val msg = "TODO: blockbattle"
+    println(msg)
+    val w = blockbattle.BlockWindow(nbrOfBlocks = (40,20))
+    w.write(msg, Pos(10,5), java.awt.Color.WHITE, 30)
