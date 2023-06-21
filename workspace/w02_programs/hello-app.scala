1a89b63f24b04d10f2e885bb03b63cda6170ea98
bjornregnell <bjorn.regnell@cs.lth.se>
2023-06-21 18:23:07 +0200

    Add workspace link to all labs, fix #683

--- workspace/w02_programs/hello-app.scala
+++ workspace/w02_programs/hello-app.scala
@@ -1,9 +1,7 @@
-//> using scala 3
+//> using scala 3.3
 
 @main def run = println("Hej Scala-app!") // en @main-funktion
 
-object Hello {
-  def main(args: Array[String]): Unit = {  // en primitiv main-metod måste finnas i ett objekt
+object Hello;
+  def main(args: Array[String]): Unit = // en primitiv main-metod måste finnas i ett objekt
     println("Hej Scala-app med primitiv main!")
-  }
-}
