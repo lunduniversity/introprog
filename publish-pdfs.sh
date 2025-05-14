1798cc507f8a202680edfd3f0d127a7c5fb3b0b4
bjornregnell <bjorn.regnell@cs.lth.se>
2025-05-14 17:27:25 +0200

    rename publish script

--- publish-pdfs.sh
+++
@@ -1,2 +0,0 @@
-echo "*** scp pdfs to web.cs.lth.se"
-scp compendium/*.pdf slides/*.pdf quickref/quickref*.pdf plan/courseplan/courseplan.pdf $LUCATID@fileadmin.cs.lth.se:/Websites/Fileadmin/pgk/.
---
+++ publish.sh
@@ -0,0 +1,38 @@
+# this script assumes that you have cloned https://github.com/lunduniversity/lunduniversity.github.io repo 
+# into the WEB_LOCAL_GITHUB dir
+
+WEB_LOCAL_GITHUB=../lunduniversity.github.io
+WEB_LOCAL_GITHUB_PATH=src/pgk/resources
+WEB_LOCAL_GITHUB_DEST=$WEB_LOCAL_GITHUB/$WEB_LOCAL_GITHUB_PATH
+
+WEB_REMOTE_LU=fileadmin.cs.lth.se
+
+HERE=$(pwd)
+
+echo "*** scp pdfs to $WEB_REMOTE_LU"
+scp compendium/*.pdf slides/*.pdf quickref/quickref*.pdf plan/courseplan/courseplan.pdf $LUCATID@$WEB_REMOTE_LU:/Websites/Fileadmin/pgk/.
+
+for f in plan/*-generated.html
+do
+  b=$(basename $f)
+  dest=$WEB_LOCAL_GITHUB_DEST/$b
+  echo "Checking changes to $f"
+  echo "  vs $dest"
+  if ! cmp -s $f $dest
+  then
+    echo "  outdated: $dest"
+    echo
+    echo "*** copy $b to local git and commit changes ***"
+    cp -i $f $dest && \
+      cd $WEB_LOCAL_GITHUB && \
+        source build.sh && \
+          git status && \
+            git add $WEB_LOCAL_GITHUB_PATH/$b && \
+              git commit -m "update $WEB_LOCAL_GITHUB_PATH/$b" && \
+                git push
+    cd $HERE
+  else
+    echo "  no changes: $f"
+  fi
+done
+
