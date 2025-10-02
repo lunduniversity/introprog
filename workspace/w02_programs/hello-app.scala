1f9e5aaf64047475ccd5c97d1f2ac6a43a9a04af
Hampus Edén <hampus.eden@gmail.com>
2025-10-02 15:34:57 +0200

    remove slides about alternative ways to write standalone applicationå

--- workspace/w02_programs/hello-app.scala
+++ workspace/w02_programs/hello-app.scala
@@ -1,7 +1,3 @@
 //> using scala 3.5
 
 @main def run = println("Hej Scala-app!") // en @main-funktion
-
-object Hello;
-  def main(args: Array[String]): Unit = // en primitiv main-metod måste finnas i ett objekt
-    println("Hej Scala-app med primitiv main!")
--- workspace/w02_programs/hello-script.sc
+++
@@ -1,5 +0,0 @@
-
-val msg = "Hejsan Scala-script!"
-println(msg)
-println("Ett Scala-script är en fil som slutar på .sc")
-println("I ett script behövs ingen @main")
