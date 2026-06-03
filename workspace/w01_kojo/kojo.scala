779e9b1c49e076d8685ad6eebd8d09f87bee1e8f
bjornregnell <bjorn.regnell@cs.lth.se>
2026-06-03 13:30:34 +0200

    nail version

--- workspace/w01_kojo/kojo.scala
+++ workspace/w01_kojo/kojo.scala
@@ -1,4 +1,4 @@
-//> using scala 3
+//> using scala 3.8.3
 //> using dep "net.kogics:kojo-lib:0.3.1,url=https://github.com/litan/kojo-lib/releases/download/v0.3.1/kojo-lib-assembly-0.3.1.jar"
 
 export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*
